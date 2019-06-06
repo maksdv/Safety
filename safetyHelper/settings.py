9ba72ab761c548d46977ce6158c8499d5d684e4d
Max <37775897+maksdv@users.noreply.github.com>
2019-06-07 01:56:15 +0200

    SK modify

--- safetyHelper/settings.py
+++ safetyHelper/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '618$ra8tp!dp!m&e_(0@g2t+v3iq9q2j*3l_l985lpo@=l@&lc'
+SECRET_KEY = '********************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
